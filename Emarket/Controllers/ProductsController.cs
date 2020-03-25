5e35c0fff3adf46c45e82d5e2999ef4243ed9285
norhan699 <nonozxp@gmail.com>
2020-03-25 10:56:42 +0200

    Filter Function

--- Emarket/Controllers/ProductsController.cs
+++ Emarket/Controllers/ProductsController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Emarket.Models;
+
 
 namespace Emarket.Controllers
 {
@@ -78,5 +80,58 @@ namespace Emarket.Controllers
             var record = db.Products.ToList();
             return View(record);
         }
+        public ActionResult Search(string key)
+        {
+            //var categoryID = (from p in db.Categories
+            //                  where p.name == key
+            //                  select new Category { id = p.id });
+            var categoryID = (from p in db.Categories
+                              where p.name == key
+                              select p.id).FirstOrDefault();
+           // int parsed = int.Parse(categoryID);
+            var listOfProducts = db.Products.Where(x => x.category_id== categoryID).ToList();
+            /* */
+            //  SqlCommand cmd = new SqlCommand(categoryID);  
+            //  int parsed = Convert.ToInt32(categoryID);
+            //  var listOfProducts = db.Products.Where(x => x.id == parsed).ToList();
+            return View(listOfProducts);
+        }
+        //public ActionResult Search(string key)
+        //{
+
+        //    IEnumerable<Category> rec;
+        //    foreach (var item in db.Categories)
+        //    {
+        //        if (item.name == key)
+        //        {
+        //             rec = from id in db.Categories
+        //                      select id;
+        //        }
+        //    }
+        //    IEnumerable<Product> se = from p in db.Products
+        //             where p.category_id == rec
+        //             select p;
+
+
+        //    return View();
+        // }
+        //    //}
+        //[ActionName("Layout")]
+        //public ActionResult Search(string key)
+        //{
+        //    //IEnumerable<Product> selection = from p in db.Products
+        //    //                                 where p.Category.name == key
+        //    //                                 select p;
+        //    //IEnumerable<Category>
+        //    var selection = from p in db.Categories
+        //                                      where p.name==key
+        //                                      select new {p.id};
+        //    var bsmallah = from q in db.Products
+        //                                    where selection.Equals(q.id)
+        //                                    select q;
+        //    return View(bsmallah);
+        //}
+
+
     }
 }
